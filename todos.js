aa44b3b11064439c7b46b39ec157ab013b098b7f
Jaewon <skyboyddd@gmail.com>
2015-03-12 18:26:29 +0900

    modified javascript code

--- todos.js
+++ todos.js
@@ -1,2 +1,29 @@
-//start...
-console.log('hello world');
+
+
+function getDom(id){
+    return document.getElementById(id);
+}
+
+var todoStringField = getDom('todoString');
+
+
+todoStringField.addEventListener('keyup' , function(event){
+
+
+    if(event.keyCode === 13){
+        console.log(todoStringField.value);
+
+        var newTodo = todoStringField.value;
+        todoStringField.value = "";
+
+
+        var listDom = getDom('todoList');
+
+        listDom.innerHTML += ' \
+        <li>\
+            <button class="delete">×</button>\
+            <input type="checkbox" class="toggle-checked">\
+            <span class="text"> '+ newTodo +' </span>\
+        </li>';
+    }
+});
